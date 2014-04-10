175dca0de2cf422922c0f8ad36035d82f2d91609
Felipe Madrigal <luis.felipe@ooyala.com>
2014-04-10 01:54:45 -0500

    Run gofmt

--- client.go
+++ client.go
@@ -1,74 +1,73 @@
 package main
 
 import (
-        "log"
-        "net"
-        "os"
-        "fmt"
-        "bufio"
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
 )
 
 func listen(conn *net.UDPConn) {
-        fmt.Println("On listener")
-        var buf []byte = make([]byte, 1500)
-        for {
-                n,address, err := conn.ReadFromUDP(buf)
+	fmt.Println("On listener")
+	var buf []byte = make([]byte, 1500)
+	for {
+		n, address, err := conn.ReadFromUDP(buf)
 
-                if err != nil {
-                        log.Println("error reading data from connection")
-                        log.Println(err)
-                        return
-                }
+		if err != nil {
+			log.Println("error reading data from connection")
+			log.Println(err)
+			return
+		}
 
-                if address != nil {
+		if address != nil {
 
-                        log.Println("got message from ", address, " with n = ", n)
+			log.Println("got message from ", address, " with n = ", n)
 
-                        if n > 0 {
-                                log.Println("from address", address, "got message:", string(buf[0:n]), n)
-                        }
-                }
-                }
+			if n > 0 {
+				log.Println("from address", address, "got message:", string(buf[0:n]), n)
+			}
+		}
+	}
 }
 
 func main() {
 
-        if len(os.Args) != 2{
-                fmt.Fprintf(os.Stderr, "Usage:%s host:port", os.Args[0])
-                os.Exit(1)
-        }
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage:%s host:port", os.Args[0])
+		os.Exit(1)
+	}
 
-        service := os.Args[1]
-        log.Println("Connecting to server at ", service)
-        conn, err := net.Dial("udp",service)
-        if err != nil {
-                log.Println("Could not resolve udp address or connect to it  on " , service)
-                log.Println(err)
-                return
-        }
-        go listen(conn.(*net.UDPConn))
-        log.Println("Connected to server at ", service)
-        defer conn.Close()
+	service := os.Args[1]
+	log.Println("Connecting to server at ", service)
+	conn, err := net.Dial("udp", service)
+	if err != nil {
+		log.Println("Could not resolve udp address or connect to it  on ", service)
+		log.Println(err)
+		return
+	}
+	go listen(conn.(*net.UDPConn))
+	log.Println("Connected to server at ", service)
+	defer conn.Close()
 
-        reader := bufio.NewReader(os.Stdin)
-        for {
-                line, err := reader.ReadString('\n')
-                if err != nil {
-                        log.Println("Error reading from stdin")
-                        continue
-                }
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			log.Println("Error reading from stdin")
+			continue
+		}
 
+		n, err := conn.Write([]byte(line))
+		if err != nil {
+			log.Println("error writing data to server", service)
+			log.Println(err)
+			return
+		}
 
-                n, err := conn.Write([]byte(line))
-                if err != nil {
-                        log.Println("error writing data to server", service)
-                        log.Println(err)
-                        return
-                }
-
-                if n > 0 {
-                        log.Println("Wrote ",n, " bytes to server at ", service)
-                }
-        }
+		if n > 0 {
+			log.Println("Wrote ", n, " bytes to server at ", service)
+		}
+	}
 
 }
--- server.go
+++ server.go
@@ -1,112 +1,111 @@
 package main
 
 import (
-        "net"
-        "log"
-        "fmt"
-        "time"
-        "bufio"
-        "os"
-        "strings"
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+	"time"
 )
 
 func main() {
 
-        port := "127.0.0.1:1200"
+	port := "127.0.0.1:1200"
 
-        udpAddress, err := net.ResolveUDPAddr("udp4",port)
+	udpAddress, err := net.ResolveUDPAddr("udp4", port)
 
-        if err != nil {
-                log.Println("error resolving UDP address on ", port)
-                log.Println(err)
-                return
-        }
+	if err != nil {
+		log.Println("error resolving UDP address on ", port)
+		log.Println(err)
+		return
+	}
 
-        conn ,err := net.ListenUDP("udp",udpAddress)
+	conn, err := net.ListenUDP("udp", udpAddress)
 
-        if err != nil {
-                log.Println("error listening on UDP port ", port)
-                log.Println(err)
-                return
-        }
-        log.Println("Got a connection")
-        defer conn.Close()
-        go getUserInput()
-        read := handle(conn)
-        for {
-                fmt.Println(read)
-        }
+	if err != nil {
+		log.Println("error listening on UDP port ", port)
+		log.Println(err)
+		return
+	}
+	log.Println("Got a connection")
+	defer conn.Close()
+	go getUserInput()
+	read := handle(conn)
+	for {
+		fmt.Println(read)
+	}
 }
 
 //This is going to be on the main loop and will basically be our user interface
 func getUserInput() {
-        reader := bufio.NewReader(os.Stdin)
-        for {
-                line, err := reader.ReadString('\n')
-                if err != nil {
-                        log.Println("Error reading from stdin")
-                        continue
-                }
-                handleUserInput(line)
-        }
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			log.Println("Error reading from stdin")
+			continue
+		}
+		handleUserInput(line)
+	}
 }
 
 // TODO this should probably get the connection too
 func handleUserInput(line string) {
-        fmt.Println(strings.TrimRight(line, "\n"))
+	fmt.Println(strings.TrimRight(line, "\n"))
 }
 
 // Each incoming connection will have a message with whatever they want to send
 // and who sent it
 type Message struct {
-        Content []byte
-        Sender *net.UDPAddr
-        Timestamp time.Time
+	Content   []byte
+	Sender    *net.UDPAddr
+	Timestamp time.Time
 }
 
 // TODO handle should know what to do when you need to become the server
 func handle(conn *net.UDPConn) <-chan Message {
-        read := listen(conn)
-        for {
-                message := <- read
-                if message.Content != nil {
-                        fmt.Println("Content", string(message.Content))
-                        fmt.Println("From address", *message.Sender)
-                        fmt.Println("In time", message.Timestamp)
-
-                }
-        }
+	read := listen(conn)
+	for {
+		message := <-read
+		if message.Content != nil {
+			fmt.Println("Content", string(message.Content))
+			fmt.Println("From address", *message.Sender)
+			fmt.Println("In time", message.Timestamp)
+		}
+	}
 }
 
 func listen(conn *net.UDPConn) <-chan Message {
-        c := make(chan Message)
-        go func() {
-                buff := make([]byte, 1024)
+	c := make(chan Message)
+	go func() {
+		buff := make([]byte, 1024)
 
-                for {
-                        n, addr, err := conn.ReadFromUDP(buff)
-                        if n > 0 && addr != nil{
-                                // Copy the response
-                                res := make([]byte, n)
+		for {
+			n, addr, err := conn.ReadFromUDP(buff)
+			if n > 0 && addr != nil {
+				// Copy the response
+				res := make([]byte, n)
 
-                                // Trim newline
-                                if (string(buff[n-1]) == "\n") {
-                                        copy(res, buff[:n-1])
-                                } else {
-                                        copy(res, buff[:n])
-                                }
+				// Trim newline
+				if string(buff[n-1]) == "\n" {
+					copy(res, buff[:n-1])
+				} else {
+					copy(res, buff[:n])
+				}
 
-                                // Create the message
-                                m := Message{Content: res,
-                                 Sender: addr,
-                                 Timestamp: time.Now()}
-                                c <- m
-                        }
-                        if err != nil {
-                                c <- Message{nil, nil, time.Now()}
-                                break
-                        }
-                 }
-        }()
-        return c
+				// Create the message
+				m := Message{Content: res,
+					Sender:    addr,
+					Timestamp: time.Now()}
+				c <- m
+			}
+			if err != nil {
+				c <- Message{nil, nil, time.Now()}
+				break
+			}
+		}
+	}()
+	return c
 }
