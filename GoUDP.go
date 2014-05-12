a963ee6bee6bac9eceefe75cd24687a51c53c038
Felipe Madrigal <luis.felipe@ooyala.com>
2014-05-11 21:06:57 -0500

    Try to use a logger to a file. For some reason is not working as expected

--- GoUDP.go
+++ GoUDP.go
@@ -126,6 +126,15 @@ func init() {
 	sendingChannel = make(chan []byte)
 	userClocks = make([]clockMessage, 1)
 	otherClientsAddress = make(map[int]bool, 1)
+
+	f, err := os.OpenFile("testlogfile", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+
+	log.SetOutput(f)
+	log.Println("This is a test log entry")
 }
 
 func main() {
